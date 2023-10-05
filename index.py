5a0fe258767bd01d2d0d9d0ca1e294586be23dc7
g12021202 <g12021202@gmail.com>
2023-10-05 14:54:39 +0800

    modify

--- index.py
+++ index.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.jinja.html')
+    return render_template("index.jinja.html")
+
+@app.route("/feature")
+def feature():
+    return render_template("feature.jinja.html")
+
+@app.route("/description")
+def description():
+    return render_template("description.jinja.html")
+
+@app.route("/product")
+def product():
+    return render_template("product.jinja.html")
